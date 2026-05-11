61a5bd75c31987bb3c7375cc49a14ab080bf1c1a
DmitriyKrylov111 <dimakrylov.2010@bk.ru>
2026-05-11 09:27:55 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,13 @@
-
+// Базовый Service Worker для кэширования
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('er-delivery-v1').then((cache) => {
+      return cache.addAll(['index.html']);
+    })
+  );
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
